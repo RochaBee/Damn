4888f66c276dcb4acb4bfc36df490cd4d2c6eec0
RochaBee <belroangel2018@gmail.com>
2023-10-05 00:03:12 -0600

    Algoritmo part1

---
+++ Unidad2/C#/Algoritmo4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algoritmo4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int celsius, fahrenheit;
+            // Solicitar al usuario la temperatura en grados Celsius
+            Console.Write("Ingrese la temperatura en grados Celsius: ");
+            celsius = int.Parse(Console.ReadLine());
+
+            // Convertir a grados Fahrenheit
+            fahrenheit = celsius * 9 / 5 + 32;
+
+            // Mostrar el resultado
+            Console.WriteLine("La temperatura en grados Fahrenheit es: " + fahrenheit);
+
+            // Esperar a que el usuario presione una tecla para salir
+            Console.ReadKey();
+        }
+    }
+}
