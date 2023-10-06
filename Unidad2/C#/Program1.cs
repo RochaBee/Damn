fc6e72c59e5e0d2be0931837e3cfa0c8c3b608b4
RochaBee <belroangel2018@gmail.com>
2023-10-05 18:02:58 -0600

    P3.jav

---
+++ Unidad2/C#/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System; //Paso 1
+
+namespace P_1 //Paso 1 
+{
+    internal class Program
+    {
+        static void Main (string[] args) //Paso 1
+        {
+            //DECLARAR VARIABLES
+            string nombre = "Angel Rocha";
+         //Imprimir nombre referenciado a una variable
+            Console.WriteLine(nombre);
+            //Cada linea debe finalizar con ; punto y coma
+        }
+    }
+}
---
+++ Unidad2/C#/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P_2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //algoritmo2
+            //Inicio
+            //Declarar variables
+            string nombre = " Roberto Gael ", apellidos = " Ochoa Cervantes ", completo;
+            completo = nombre + apellidos;
+            Console.WriteLine("* * Bienvenido * * " + completo + " * * " );
+            int N1, N2, suma;
+            Console.WriteLine("introducir primer numero");
+            N1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("introducir segundo numero");
+            N2 = Int32.Parse(Console.ReadLine());
+            suma = N1 + N2;
+            Console.WriteLine(suma);
+            Console.WriteLine(" -- Gracias :)");
+        }
+    }
+}
