37143db1c8e697007dff1072266cde089915addc
Sieri21 <146492506+Sieri21@users.noreply.github.com>
2023-10-25 17:55:02 -0600

    update

---
+++ Unidad3/Java/U3p15.java
@@ -0,0 +1,12 @@
+package com.mycompany.u3p15;
+
+public class U3p15 {
+
+    public static void main(String[] args) {
+        int c = 1;
+        while (c<=20) {
+            System.out.println(c);
+            c++;
+        }
+    }
+}
